785f7b18dd442c0860e93327a522738aa3a6d518
jredfox <jjredfox@hotmail.com>
2019-11-02 06:04:45 -0500

    removed hardcoded shit

--- src/main/java/com/evilnotch/menulib/MenuLib.java
+++ src/main/java/com/evilnotch/menulib/MenuLib.java
@@ -16,7 +16,7 @@ import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.relauncher.Side;
 
-@Mod(modid = MenuLib.MODID,name = MenuLib.NAME, version = MenuLib.VERSION, clientSideOnly = true, dependencies = "required-after:evilnotchlib;after:custommainmenu")
+@Mod(modid = MenuLib.MODID, name = MenuLib.NAME, version = MenuLib.VERSION, clientSideOnly = true, dependencies = "required-after:evilnotchlib")
 public class MenuLib {
 	
 	public static final String MODID = "menulib";
