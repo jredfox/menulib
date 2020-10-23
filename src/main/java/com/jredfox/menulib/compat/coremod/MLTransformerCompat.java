793300b015100b490bd47f28adc2843ac131a49b
jredfox <jredfox@hotmail.com>
2020-10-23 18:18:33 -0500

    fix cmm json hook

--- src/main/java/com/jredfox/menulib/compat/coremod/MLTransformerCompat.java
+++ src/main/java/com/jredfox/menulib/compat/coremod/MLTransformerCompat.java
@@ -1,19 +1,22 @@
 package com.jredfox.menulib.compat.coremod;
 
-import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.tree.AbstractInsnNode;
 import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.InsnList;
+import org.objectweb.asm.tree.MethodInsnNode;
 import org.objectweb.asm.tree.MethodNode;
+import org.objectweb.asm.tree.VarInsnNode;
 
-import com.evilnotch.lib.asm.FMLCorePlugin;
-import com.evilnotch.lib.asm.classwriter.MCWriter;
 import com.evilnotch.lib.asm.util.ASMHelper;
 import com.jredfox.menulib.coremod.MLTransformer;
 
 import net.minecraft.launchwrapper.IClassTransformer;
 /**
- * made to fix some CMM bugs
+ * made to fix some CMM bugs & add a json hook
  * A: remove it from overriding all other mods
  * B: fix several issues in GuiCustom one with wrapped button ids, the other with firing button events so it shows the current gui instead of a fake
+ * C: add a json hook so modders can add their wrapped button for cmm main menu fires everytime the json file is deleted creating a new default
  */
 public class MLTransformerCompat implements IClassTransformer{
 	
@@ -31,7 +34,7 @@ public class MLTransformerCompat implements IClassTransformer{
 		if(name.equals("lumien.custommainmenu.handler.CMMEventHandler"))
 		{
 			System.out.println("transforming cmm:" + name);
-			return ASMHelper.replaceClass(MLTransformer.getInputBase() + "CMMEventHandler");
+			return ASMHelper.replaceClass(MLTransformer.getInputBase() + "CMMEventHandler");//replaced as I re-wrote most of it
 		}
 		else if(name.equals("lumien.custommainmenu.gui.GuiCustom"))
 		{
@@ -40,9 +43,11 @@ public class MLTransformerCompat implements IClassTransformer{
 			patchGuiCustom(node, MLTransformer.getInputBase());
 			return MLTransformer.writeClass(node, name);
 		}
-		else if(name.equals(""))
+		else if(name.equals("lumien.custommainmenu.configuration.ConfigurationLoader"))
 		{
-			
+			ClassNode node = ASMHelper.getClassNode(bytes);
+			hookConfigurationLoader(node);
+			return MLTransformer.writeClass(node, name);
 		}
 		}
 		catch(Throwable t)
@@ -50,5 +55,24 @@ public class MLTransformerCompat implements IClassTransformer{
 			t.printStackTrace();
 		}
 		return bytes;
+	}
+
+	public static void hookConfigurationLoader(ClassNode classNode)
+	{
+		MethodNode method = ASMHelper.getMethodNode(classNode, "load", "()V");
+		AbstractInsnNode spot = null;
+		MethodInsnNode m = new MethodInsnNode(Opcodes.INVOKESTATIC, "com/google/common/io/ByteStreams", "copy", "(Ljava/io/InputStream;Ljava/io/OutputStream;)J", false);
+		for(AbstractInsnNode ab : method.instructions.toArray())
+		{
+			if(ab instanceof MethodInsnNode && ASMHelper.equals(m, (MethodInsnNode) ab) && ab.getNext().getOpcode() == Opcodes.POP2)
+			{
+				spot = ab;
+				break;
+			}
+		}
+		InsnList list = new InsnList();
+		list.add(new VarInsnNode(Opcodes.ALOAD, 3));
+		list.add(new MethodInsnNode(Opcodes.INVOKESTATIC, "com/jredfox/menulib/compat/event/CMMJsonRegistry", "fire", "(Ljava/io/File;)V", false));
+		method.instructions.insert(spot, list);
 	}
 }
--- src/main/java/com/jredfox/menulib/compat/event/CMMJsonRegistry.java
+++ src/main/java/com/jredfox/menulib/compat/event/CMMJsonRegistry.java
@@ -1,10 +1,14 @@
 package com.jredfox.menulib.compat.event;
 
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.ralleytn.simple.json.JSONObject;
+import org.ralleytn.simple.json.JSONParseException;
 
 import com.evilnotch.lib.util.JavaUtil;
 
@@ -17,12 +21,37 @@ public class CMMJsonRegistry {
 	
 	public static void fire(File mainmenu)
 	{
-		JSONObject json = JavaUtil.getJson(mainmenu);
+		JSONObject json = getJson(mainmenu);
 		for(ICMMJsonEvent cmm : registry)
 		{
 			cmm.fire(json);
 		}
 		JavaUtil.saveJSON(json, mainmenu, true);
+	}
+	
+	public static JSONObject getJson(File armor) 
+	{
+		FileReader reader = null;
+	      try 
+	      {
+	    	  reader = new FileReader(armor);
+	         JSONObject json = (JSONObject)JavaUtil.jsonParser.parse((Reader)(reader));
+	         return json;
+	      } 
+	      catch (JSONParseException | IOException var2) 
+	      {
+	         var2.printStackTrace();
+	         return null;
+	      }
+	      finally
+	      {
+	    	  if(reader != null)
+				try {
+					reader.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+	      }
 	}
 
 }
